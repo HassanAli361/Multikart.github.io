6b07b602812b659e00fbba6f80b8c9e92148f529
Hassan Ali <hassanali4u2003@gmail.com>
2022-09-28 04:55:40 -0700

    sdf

--- src/router/routes.js
+++ src/router/routes.js
@@ -18,7 +18,6 @@ const routes = [
     path: '/login',
     component: () => import('layouts/LoginLayout.vue'),
     children: [
-      { path: '/login', component: () => import('pages/login.vue') },
     ]
   },
 
